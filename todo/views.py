26a2f15f6a25316775ceb0da7c7dbc0cb41a9ad6
Deirdre18 <deirdreweldon18@gmail.com>
2019-10-19 11:26:06 +0100

    renamed todo/templates/todo_lists.html

--- todo/views.py
+++ todo/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def get_todo_list(request):
-    return render(request, "todo_lists.html")
+    return render(request, "todo_list.html")
