4b90380022d6f45faf7d4515163886dc1897c4b9
Deirdre18 <deirdreweldon18@gmail.com>
2019-10-21 11:58:46 +0100

    added new file item_form.html to todo/templates, modified urls.py-django_todo, todo_list.html-todo/templates,views.py-todo

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
 
 # Create your views here.
@@ -7,3 +7,15 @@ def get_todo_list(request):
     return render(request, "todo_list.html", {
         'items': results
     })
+#django will return a dictionary, which is the form
+def create_an_item(request):
+    if request.method == "POST":
+        new_item = Item()
+        new_item.name = request.POST.get('name')
+        #this is so that value for 'done' is saved to db
+        new_item.done = 'done' in request.POST
+        new_item.save()
+
+        return redirect(get_todo_list)
+
+    return render(request, "item_form.html")
