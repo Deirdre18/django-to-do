4b34b1aceebc168d7e6c0963bbf2348ac307b3e3
Deirdre18 <deirdreweldon18@gmail.com>
2019-10-21 14:56:06 +0100

    added forms.py, modified item_form.html(todo/temmplates),views.py(todo)

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 def get_todo_list(request):
@@ -10,12 +11,14 @@ def get_todo_list(request):
 #django will return a dictionary, which is the form
 def create_an_item(request):
     if request.method == "POST":
-        new_item = Item()
-        new_item.name = request.POST.get('name')
-        #this is so that value for 'done' is saved to db
-        new_item.done = 'done' in request.POST
-        new_item.save()
+        #creating new form and inside constructor of form, populating with values from POST request item
+        form = ItemForm(request.POST, request.FILES)
+        #django will do automatic check that form is valid
+        if form.is_valid():
+            #form automatically knows form is valid and where to be saved, becaused of adding meta class
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm()
 
-        return redirect(get_todo_list)
-
-    return render(request, "item_form.html")
+    return render(request, "item_form.html", {'form': form})
