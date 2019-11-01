14f630c509594c128f631cea0fc223dd414d24a2
Deirdre18 <deirdreweldon18@gmail.com>
2019-11-01 18:09:54 +0000

    added new files 'tests.py' and requirements.txt

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,12 @@
+# TestCase is the base object that we will inherit
 from django.test import TestCase
 
-# Create your tests here.
+# creating tests -adding a new class called TestDjango and this will inherit from TestCase and what we need to do is define a method on this class and what we want to say is test_is_this_thing_on and this will just run a little red green refactor type test
+
+
+class TestDjango(TestCase):
+
+    # this test passed when 'python3 manage.py test was run' and database destroyed, so as not to dirty future tests
+    # tests must begin with 'test underscore (test_), otherwise they won't run
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
