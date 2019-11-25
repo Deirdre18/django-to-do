a3d7c48547be52d9e30e823e38726f8bd9165375
Deirdre18 <deirdreweldon18@gmail.com>
2019-11-25 20:28:51 +0000

    updated test_views.py (added more tests).

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,6 +1,7 @@
 
 # creating class. In order to test our views, what we need to do is we need to ensure that we were going to the correct location i.e. the correct URL and the way to do that is to create test_views using Django framework.
 from django.test import TestCase
+from .models import Item
 
 # creating class which will inherit from TestCase
 
@@ -13,3 +14,20 @@ class TestViews(TestCase):
         # self.assertEqual(page.status_code, 200) and will check the status code property of the page object and test that it is 200
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "todo_list.html")
+
+    def test_get_add_item_page(self):
+        page = self.client.get("/add")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_item_page(self):
+        item = Item(name="Create a Test")
+        item.save()
+
+        page = self.client.get("/edit/{0}".format(item.id))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "item_form.html")
+
+    def test_get_edit_page_for_item_that_does_not_exist(self):
+        page = self.client.get("/edit/1")
+        self.assertEqual(page.status_code, 404)
