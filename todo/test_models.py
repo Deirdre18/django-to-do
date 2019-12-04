483278c2ebfa12cb77df22b8b1f08c2028fd3477
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-04 10:59:52 +0000

    modified todo/models.py

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,46 +1,21 @@
-# TestCase is the base object that we will inherit
-from django.test import TestCase
-# substatiating it from the item form this means that we need to just import our item form
 from .models import Item
-
-# creating tests -adding a new class called TestDjango and this will inherit from TestCase and what we need to do is define a method on this class and what we want to say is test_is_this_thing_on and this will just run a little red green refactor type test
-
-# no longer using 'class TestDjango(TestCase):'
-# changng test item as per below
+from django.test import TestCase
 
 
 class TestItemModel(TestCase):
 
-    # this test passed when 'python3 manage.py test was run' and database destroyed, so as not to dirty future tests
-    # tests must begin with 'test underscore (test_), otherwise they won't run
-    # no longer using 'def test_is_this_thing_on(self):'
-
-    # testing can create item with just a name
     def test_done_defaults_to_False(self):
-        item = Item(name="Create a test")
+        item = Item(name="Create a Test")
         item.save()
-        self.assertEqual(item.name, "Create a test")
-        # changing argument to False and getting rid of Equal, running just 1 check
+        self.assertEqual(item.name, "Create a Test")
         self.assertFalse(item.done)
-        # self.assertEqual(item.done, False)
 
     def test_can_create_an_item_with_a_name_and_status(self):
-        # running 2 arguments, to test when done=True
-        item = Item(name="Create a test", done=True)
+        item = Item(name="Create a Test", done=True)
         item.save()
-        self.assertEqual(item.name, "Create a test")
+        self.assertEqual(item.name, "Create a Test")
         self.assertTrue(item.done)
-        # self.assertEqual(item.done,True)
-
-    def test_can_create_an_item_with_a_name_and_status(self):
-        # running 2 arguments, to test when done=True
-        item = Item(name="Create a test", done=True)
-        item.save()
-        self.assertEqual(item.name, "Create a test")
-        self.assertTrue(item.done)
-        # self.assertEqual(item.done,True)
 
     def test_item_as_a_string(self):
-        # creating test which is equal to item as a string
-        item = Item(name="Create a test")
-        self.assertEqual("Create a test", str(item))
+        item = Item(name="Create a Test")
+        self.assertEqual("Create a Test", str(item))
