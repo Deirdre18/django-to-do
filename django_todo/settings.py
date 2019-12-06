5185cb7394e546c4608cf5ac3736866dc5b6090f
Deirdre18 <deirdreweldon18@gmail.com>
2019-12-06 16:00:39 +0000

    changes to django_todo/settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '6x8bey2jo*+xu*x=5sxd6l#4j-(d&+sx%p0p@(8b@agm%*vs+2'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
-                 'https://test-django-todo.herokuapp.com/'']
+                 'test-django-todo.herokuapp.com']
 
 
 # Application definition
